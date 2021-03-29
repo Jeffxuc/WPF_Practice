74c0477e9ec2a1932e628815d314797fc7519409
jeffxu <xujun16year@163.com>
2021-03-29 23:10:11 +0800

    add some new content

---
+++ WPF_01/Pr60_TestFunction/F01_UIQuickTips/QuickTip.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pr60_TestFunction.F01_UIQuickTips
+{
+    /// <summary>
+    /// Interaction logic for QuickTip.xaml
+    /// </summary>
+    public partial class QuickTip : Page
+    {
+        public QuickTip()
+        {
+            InitializeComponent();
+        }
+    }
+}

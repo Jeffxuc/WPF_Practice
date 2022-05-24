976a94fb93240f77e2c70f8fc3f11ea4f35d4c7d
JeffXu <xujun16year@163.com>
2022-05-24 14:50:02 +0800

    Add MVVM practice project

---
+++ WPF_01/Pr61_MVVMPractice/View/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Pr61_MVVMPractice.ViewModel;
+
+namespace Pr61_MVVMPractice.View
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        MainPage_VM mainPage_VM = new MainPage_VM();
+        public MainPage()
+        {
+            this.DataContext = mainPage_VM;
+
+            mainPage_VM.Age = 20;
+
+            InitializeComponent();
+
+            RegisterEvent();
+            
+        }
+
+        private void RegisterEvent()
+        {
+            // btn01, btn02 为MVVM模式下进行的动作。
+            btn01.Click += (s1, e1) =>
+            {
+                mainPage_VM.Age = 26;
+                mainPage_VM.City = "ShangHai";
+            };
+
+            btn02.Click += (s2, e2) =>
+            {
+                mainPage_VM.City = "Hangzhou";
+                mainPage_VM.Age = 29;
+            };
+
+            // btn03 为普通的模式，非MVVM对应的模式。
+            btn03.Click += (s3, e3) =>
+            {
+                if (txt03.Text == "Not MVVM model")
+                    txt03.Text = "Just a normal model";
+                else
+                    txt03.Text = "Not MVVM model";
+            };
+
+        }
+    }
+}
---
+++ WPF_01/Pr61_MVVMPractice/ViewModel/MainPage_VM.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr61_MVVMPractice.ViewModel
+{
+    class MainPage_VM: ViewModelBase
+    {
+        private string city="suzhou";
+        public string City
+        {
+            get { return city; }
+            set
+            {
+                if(value!=city)
+                {
+                    city = value;
+                    OnPropertyChanged();
+                }
+
+            }
+        }
+
+        private int age;
+        public int Age
+        {
+            get {return age;}
+            set
+            {
+                if(value!=age)
+                {
+                    age = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+    }
+}

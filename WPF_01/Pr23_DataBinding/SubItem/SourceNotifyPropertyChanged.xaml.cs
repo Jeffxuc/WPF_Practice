450b6bc186e60c2cdce073bb2aa708c6d899c8d3
JeffXu <xujun16year@163.com>
2022-09-15 10:51:38 +0800

    Update DataBinding content

--- WPF_01/Pr23_DataBinding/SubItem/SourceNotifyPropertyChanged.xaml.cs
+++ WPF_01/Pr23_DataBinding/SubItem/SourceNotifyPropertyChanged.xaml.cs
@@ -22,24 +22,38 @@ namespace Pr23_DataBinding.SubItem
     public partial class SourceNotifyPropertyChanged : Window
     {
         NormalData myData = new NormalData();
+        static int count = 0;
 
         public SourceNotifyPropertyChanged()
         {
-            this.DataContext = new NormalData();
+            this.DataContext = myData;  // Attention: DataContext 一定是上面所 New 出来的，或者是XAML中实例的
 
             InitializeComponent();
 
-            //在后台实现对自定义属性的绑定
+            //在后台实现对自定义属性的绑定：单一绑定
             Binding binding = new Binding();
             binding.Source = myData;
             binding.Path = new PropertyPath("Num");
             binding.Mode = BindingMode.OneWay;
             txt00.SetBinding(TextBlock.TextProperty, binding);
+
+
+            // 后台 Code 实现: 多重绑定，必须要设置绑定的StringFormat 或者 Converter
+            MultiBinding multiBinding = new MultiBinding();
+            multiBinding.StringFormat = "{0} ++++ {1}";
+            multiBinding.Bindings.Add(new Binding("Msg_errorinfo") { Source = myData });
+            multiBinding.Bindings.Add(new Binding("Msg_errorCode") { Source = myData });
+            tb31.SetBinding(TextBlock.TextProperty, multiBinding);
+
         }
 
         private void Btn01_Click(object sender, RoutedEventArgs e)
         {
+            count++;
+
             myData.Num++;
+
+            myData.Msg_errorCode = count.ToString();
         }
     }
 
@@ -51,6 +65,9 @@ namespace Pr23_DataBinding.SubItem
         {
             Num = 88;
             city = "suzhou";
+
+            Msg_errorCode = "0";
+            Msg_errorinfo = "Show error infomations.";
         }
 
 
@@ -86,6 +103,34 @@ namespace Pr23_DataBinding.SubItem
                 if(city !=value)
                 {
                     city = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private string msg_errorInfo;
+        public string Msg_errorinfo
+        {
+            get { return msg_errorInfo; }
+            set
+            {
+                if(msg_errorInfo!=value)
+                {
+                    msg_errorInfo = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private string msg_errorCode;
+        public string Msg_errorCode
+        {
+            get { return msg_errorCode; }
+            set
+            {
+                if(msg_errorCode!=value)
+                {
+                    msg_errorCode = value;
                     NotifyPropertyChanged();
                 }
             }
